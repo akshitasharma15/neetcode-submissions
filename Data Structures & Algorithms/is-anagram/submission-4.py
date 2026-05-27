84eecc75d5227138323e66e3f18f0630e8c553c3
akshitasharma15 <37779553+akshitasharma15@users.noreply.github.com>
2026-05-27 22:00:00 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        unique_s={}
+        unique_t={}
+
+        for i, j in enumerate(s):
+            if j in unique_s:
+                unique_s[j]+= 1
+            else:
+                unique_s.update({j:1})
+        
+        for i,j in enumerate(t):
+            if j in unique_t:
+                unique_t[j] += 1
+            else:
+                unique_t.update({j:1})  
+
+        if unique_s == unique_t:
+            return True
+
+        return False     
+
+
+        
