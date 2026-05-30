ad3e844bd64b5d59096d9d6c01b0f8ed48ea3b90
akshitasharma15 <37779553+akshitasharma15@users.noreply.github.com>
2026-05-30 16:27:33 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        word =''
+        for i in strs:
+            length = len(i)
+            word = word + str(length) + '+' + i
+        print(word)
+        return word
+        
+
+    def decode(self, s: str) -> List[str]:
+        decoded_list =[]
+        i =0 
+        digit = ''
+        while i < len(s):
+            if s[i] == '+':
+                decoded_list.append(s[i+1: i + int(digit) + 1])
+                i += 1 + int(digit) 
+                digit= ''
+            else:
+                digit += s[i]
+                i += 1
+        
+        return decoded_list
+        
+
+
+
